aa389c116f563a856f502f6da3c799439badd0f6
DeFidelity <adeyemihaki@gmail.com>
2022-03-13 15:15:01 +0100

    configured whitenoise and fixed some other problems

--- landing/views.py
+++ landing/views.py
@@ -4,4 +4,4 @@ from django.views import View
 
 class Index(View):
     def get(self,request, *args,**kwargs):
-        return render(request,'landing\index.html')
+        return render(request,'/landing/index.html')
